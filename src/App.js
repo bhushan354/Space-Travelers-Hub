d44cf015b0d22101e6207adf04969a2945dd967b
bhushan354 <deshmukhbhushan354@gmail.com>
2023-10-03 23:41:59 +0530

    old navbar commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { NavLink, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import MyProfile from './components/MyProfile';
 import Missions from './components/Missions';
 import Rockets from './components/Rockets';
@@ -7,24 +7,27 @@ import Rockets from './components/Rockets';
 function App() {
   return (
     <>
-      <header>
-        <div className="title">
-          <div className="container-title">
-            <h1 className="store">Space X</h1>
-            <ul>
-              <li>
-                <NavLink to="/" className="current">Home</NavLink>
-              </li>
-              <li>
-                <NavLink to="/rockets" className="current">Rockets</NavLink>
-              </li>
-              <li>
-                <NavLink to="/missions" className="current">Missions</NavLink>
-              </li>
-            </ul>
+      <nav>
+        <div className="navContainer">
+          <div>
+            <li className="heading">
+              Space Travelers Hub
+            </li>
+          </div>
+
+          <div className="linksContainer">
+            <li>
+              <Link to="/rockets">Rockets</Link>
+            </li>
+            <li>
+              <Link to="/missions">Missions</Link>
+            </li>
+            <li>
+              <Link to="/">MyProfile</Link>
+            </li>
           </div>
         </div>
-      </header>
+      </nav>
       <div className="container">
         <Routes>
           <Route path="/rockets" element={<Rockets />} />
