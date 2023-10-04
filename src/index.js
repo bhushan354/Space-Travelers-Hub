fe7d7d562b97a93fa2826675510bbc57695602e7
izebiafe <afimiafidelis95@gmail.com>
2023-10-04 14:37:57 +0100

    add react-bootstrap

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import ReactDOM from 'react-dom/client';
+import store from './redux/store';
+import './index.css';
 import App from './App';
-import missionsSlice from './redux/Missions/missionsSlice';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider missionsSlice={missionsSlice}>
-    <App />
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
