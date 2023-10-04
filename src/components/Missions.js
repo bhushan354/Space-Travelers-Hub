f2d0bd8dd3b92222d193dc7b209037d3452c2f6b
izebiafe <afimiafidelis95@gmail.com>
2023-10-04 14:28:05 +0100

    Add missions data in element

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,97 +1,45 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import classNames from 'classnames';
-import {
-  fetchMissions,
-  joinMission,
-  leaveMission,
-} from '../redux/Missions/missionsSlice';
-import '../App.css';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { Table } from 'react-bootstrap';
+import { fetchMissionsData } from '../redux/missions/missionsSlice';
 
-function Missions() {
+const MissionTable = () => {
+  const missionsData = useSelector((state) => state.missions.missionsData);
+  const missionsStatus = useSelector((state) => state.missions.missionsStatus);
   const dispatch = useDispatch();
-  const missions = useSelector((state) => state.missions.missions);
-  const [activeMembers, setActiveMembers] = useState({});
-
   useEffect(() => {
-    dispatch(fetchMissions());
+    dispatch(fetchMissionsData());
   }, [dispatch]);
 
-  const handleJoinMission = (missionId) => {
-    dispatch(joinMission(missionId));
-    setActiveMembers({ ...activeMembers, [missionId]: true });
-  };
+  if (missionsStatus === 'loading') {
+    return <div>Loading...</div>;
+  }
 
-  const handleLeaveMission = (missionId) => {
-    dispatch(leaveMission(missionId));
-    setActiveMembers({ ...activeMembers, [missionId]: false });
-  };
+  if (missionsStatus === 'failed') {
+    return <div>Error: Unable to fetch data for the missions</div>;
+  }
 
-  const getMembershipLabel = (mission) => (activeMembers[mission.mission_id] ? 'ACTIVE MEMBER' : 'NOT A MEMBER');
-
-  const getButtonStyles = (mission) => {
-    if (activeMembers[mission.mission_id]) {
-      return {
-        background: 'transparent',
-        border: '1px solid red',
-        color: 'red',
-      };
-    }
-    return {
-      background: 'transparent',
-      border: '1px solid rgb(95, 92, 92)',
-      color: 'rgb(95, 92, 92)',
-    };
-  };
   return (
-    <div className="missions-container">
-      <table className="missions-table">
-        <thead>
-          <tr>
-            <th>Mission</th>
-            <th>Description</th>
-            <th>Status</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {missions.map((mission) => (
-            <tr
-              key={mission.mission_id}
-              className={classNames({
-                active: activeMembers[mission.mission_id],
-              })}
-            >
-              <td className="mission-name">{mission.mission_name}</td>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>Mission</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {missionsData &&
+          missionsData.map((mission) => (
+            <tr key={mission.mission_id}>
+              <td>{mission.mission_name}</td>
               <td>{mission.description}</td>
-              <td>
-                <p
-                  className={classNames('membership-label', {
-                    'active-member-label': activeMembers[mission.mission_id],
-                  })}
-                >
-                  {getMembershipLabel(mission)}
-                </p>
-              </td>
-              <td>
-                <button
-                  type="button"
-                  className="action-button"
-                  style={getButtonStyles(mission)}
-                  onClick={() => (activeMembers[mission.mission_id]
-                    ? handleLeaveMission(mission.mission_id)
-                    : handleJoinMission(mission.mission_id))}
-                >
-                  {activeMembers[mission.mission_id]
-                    ? 'Leave Mission'
-                    : 'Join Mission'}
-                </button>
-              </td>
+              <td>Upcoming</td>
             </tr>
           ))}
-        </tbody>
-      </table>
-    </div>
+      </tbody>
+    </Table>
   );
-}
-export default Missions;
+};
+
+export default MissionTable;
