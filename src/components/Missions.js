d12d12acf8a6ae4d7cd39fc8921b5f9ab0d4c43e
bhushan354 <deshmukhbhushan354@gmail.com>
2023-10-06 08:16:42 +0530

    good commit

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,52 +1,94 @@
 import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Table } from 'react-bootstrap';
-import { fetchMissionsData } from './Redux/Missions/missionsSlice';
+import PropTypes from 'prop-types';
+import { useDispatch, useSelector } from 'react-redux';
+import { missionsHandler, getMissionsData } from './Redux/Missions/missionsSlice';
 
-const LoadingView = () => <div>Loading...</div>;
-const ErrorView = () => <div>Error: Unable to fetch data for the missions</div>;
-
-const MissionTable = () => {
-  const missionsData = useSelector((state) => state.missions.missionsData);
-  const missionsStatus = useSelector((state) => state.missions.missionsStatus);
+function MisionItem({
+  id, name, description, reserved,
+}) {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(fetchMissionsData());
-  }, [dispatch]);
-
-  if (missionsStatus === 'loading') {
-    return <LoadingView />;
-  }
-
-  if (missionsStatus === 'failed') {
-    return <ErrorView />;
-  }
-
-  const renderMissionRow = (mission) => (
-    <tr key={mission.mission_id}>
-      <td>{mission.mission_name}</td>
-      <td>{mission.description}</td>
-      <td>Upcoming</td>
-      <td>
-        <button type="button">Join Mission</button>
-      </td>
-    </tr>
-  );
+  const getButton = (reserved, btn) => {
+    let button;
+    if (btn === 'member') {
+      button = reserved ? (
+        <span className="active-member-badge">Active Member</span>
+      ) : (
+        <span className="not-member-badge">Not a member</span>
+      );
+    }
+    if (btn === 'mission') {
+      button = reserved ? (
+        <button className="leave-mission-btn" type="button" onClick={() => dispatch(missionsHandler(id))}>Leave Mission</button>
+      ) : (
+        <button className="join-mission-btn" type="button" onClick={() => dispatch(missionsHandler(id))}>Join Mission</button>
+      );
+    }
+    return button;
+  };
 
   return (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Mission</th>
-          <th>Description</th>
-          <th>Status</th>
-        </tr>
-        <th> </th>
-      </thead>
-      <tbody>{missionsData && missionsData.map(renderMissionRow)}</tbody>
-    </Table>
+    <>
+      <td className="mission-name">{name}</td>
+      <td className="mission-description">{description}</td>
+      <td className="table-badges">{getButton(reserved, 'member')}</td>
+      <td className="table-btns">{getButton(reserved, 'mission')}</td>
+    </>
   );
+}
+
+MisionItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
-export default MissionTable;
+function Missions() {
+  const dispatch = useDispatch();
+  const { missions, pending, error } = useSelector((store) => store.missions);
+
+  useEffect(() => {
+    if (missions.length < 1) {
+      dispatch(getMissionsData());
+    }
+  }, [dispatch, missions.length]);
+
+  let content;
+
+  if (!pending && !error) {
+    content = (
+      <table className="missions-table">
+        <tbody>
+          <tr key="missions">
+            <th>Mission</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>{' '}</th>
+          </tr>
+          {missions.map((mission) => (
+            <tr key={mission.id}>
+              <MisionItem
+                name={mission.name}
+                description={mission.description}
+                reserved={mission.reserved}
+                id={mission.id}
+              />
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+
+  if (pending) {
+    content = <h1>Fetching Missions</h1>;
+  }
+  if (error) {
+    content = <h1>Error occurred while fetching missions</h1>;
+  }
+
+  return <section className="missions">{content}</section>;
+}
+
+export default Missions;
--- src/components/MyProfile.js
+++ src/components/MyProfile.js
@@ -1,9 +1,33 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 function MyProfile() {
+  // reserved rockets
+  const { rocketsData } = useSelector((state) => state.rocketsData);
+  const reservedRockets = rocketsData.filter((rocket) => rocket.isReserved === true);
+
+  const { missions } = useSelector((state) => state.missions);
+  const activeMissions = missions.filter((rocket) => rocket.reserved === true);
+
   return (
-    <div className="MyProfile">
-      <h2>Welcome To My-Profile</h2>
+    <div className="mainContainer">
+      <div className="rocketsContainer">
+        <h2>My Reserved Rockets</h2>
+        <ol className="allReservedContainer">
+          {reservedRockets.map((rocket) => (
+            <li className="reservedRocketLi" key={rocket.id}>{rocket.name}</li>
+          ))}
+        </ol>
+      </div>
+      {/* your joined missions here */}
+      <div className="missionsContainer">
+        <h2>My Reserved Missions</h2>
+        <ol className="allActiveContainer">
+          {activeMissions.map((rocket) => (
+            <li className="reservedMissionsLi" key={rocket.id}>{rocket.name}</li>
+          ))}
+        </ol>
+      </div>
     </div>
   );
 }
